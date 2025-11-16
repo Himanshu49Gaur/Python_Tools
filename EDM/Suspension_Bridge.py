71481c0dbe5d1bff97cb934fa8aecebaf2f4da29
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-11-17 01:18:33 +0530

    Update Suspension_Bridge.py

--- EDM/Suspension_Bridge.py
+++ EDM/Suspension_Bridge.py
@@ -1,1 +1,18 @@
 import turtle
+
+def draw_tower(t, x, y, height, width, color="darkgray"):
+    t.penup()
+    t.goto(x - width/2, y)
+    t.pendown()
+    t.color(color)
+    t.pensize(3)
+    t.begin_fill()
+    t.forward(width)
+    t.left(90)
+    t.forward(height)
+    t.left(90)
+    t.forward(width)
+    t.left(90)
+    t.forward(height)
+    t.left(90)
+    t.end_fill()
