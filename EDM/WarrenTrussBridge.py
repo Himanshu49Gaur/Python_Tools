610cff6f5d642df685f48b2b57afd21251757e5d
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-11-21 00:49:14 +0530

    Update WarrenTrussBridge.py

--- EDM/WarrenTrussBridge.py
+++ EDM/WarrenTrussBridge.py
@@ -1,2 +1,13 @@
 import turtle 
 
+def draw_member(t, x1, y1, x2, y2, thick=3):
+    t.penup()
+    t.goto(x1, y1)
+    t.pendown()
+    t.pensize(thick)
+    t.goto(x2, y2)
+    # Draw joint node
+    t.penup()
+    t.goto(x2, y2 - 3)
+    t.pendown()
+    t.circle(3)
