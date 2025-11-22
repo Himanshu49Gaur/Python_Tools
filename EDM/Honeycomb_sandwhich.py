b680be23e69fd39c24302d290e185a2c68b87786
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-11-22 23:23:06 +0530

    Update Honeycomb_sandwhich.py

--- EDM/Honeycomb_sandwhich.py
+++ EDM/Honeycomb_sandwhich.py
@@ -1,2 +1,14 @@
 import turtle 
 import math
+
+def draw_hexagon(t, x, y, size, color):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    t.color("black", color)
+    t.pensize(1)
+    t.begin_fill()
+    for _ in range(6):
+        t.forward(size)
+        t.left(60)
+    t.end_fill()
