0956f0a178ed5e5d876b2b6e76e775d94997a647
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-11-26 11:36:52 +0530

    Update Kelvin_Wake.py

--- EDM/Kelvin_Wake.py
+++ EDM/Kelvin_Wake.py
@@ -1,3 +1,11 @@
 import turtle 
 import math
 
+def draw_boat(t, x, y):
+    t.penup(); t.goto(x, y); t.pendown()
+    t.color("black"); t.fillcolor("brown"); t.begin_fill()
+    t.setheading(0)
+    t.forward(40)
+    t.left(150); t.forward(50)
+    t.left(60); t.forward(50)
+    t.end_fill()
