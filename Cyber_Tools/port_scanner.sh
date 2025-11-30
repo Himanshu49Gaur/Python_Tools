84c910bfbc9f756489e1327ca92d526a6991200c
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-11-30 18:19:18 +0530

    Update port_scanner.sh

--- Cyber_Tools/port_scanner.sh
+++ Cyber_Tools/port_scanner.sh
@@ -3,3 +3,6 @@
 echo "=============================================="
 echo "    Advanced Multi-threaded Port Scanner      "
 echo "=============================================="
+
+# Get target IP or Domain
+read -p "Enter target IP or Domain: " target
