726ae86c1ee9cc37ed45981b90d95401aaac8eec
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-11-30 18:21:27 +0530

    Update port_scanner.sh

--- Cyber_Tools/port_scanner.sh
+++ Cyber_Tools/port_scanner.sh
@@ -10,3 +10,6 @@ read -p "Enter target IP or Domain: " target
 # Get port range
 read -p "Enter Start Port: " start_port
 read -p "Enter End Port: " end_port
+
+# Get output filename
+read -p "Enter output file name (e.g. results.txt): " outfile
