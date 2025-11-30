a60c9d0762709ac8fce55aa81ca65000e2033ad5
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-11-30 18:20:30 +0530

    Update port_scanner.sh

--- Cyber_Tools/port_scanner.sh
+++ Cyber_Tools/port_scanner.sh
@@ -6,3 +6,7 @@ echo "=============================================="
 
 # Get target IP or Domain
 read -p "Enter target IP or Domain: " target
+
+# Get port range
+read -p "Enter Start Port: " start_port
+read -p "Enter End Port: " end_port
