1bf1df1bb3c7af15f2c22d9402c2eb98cc06e3a5
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-02 23:34:38 +0530

    Update Lexical_Analyzer.c

--- TOCC/Lexical_Analyzer.c
+++ TOCC/Lexical_Analyzer.c
@@ -9,3 +9,43 @@ typedef struct {
     TokenType type;
     char value[100];
 } Token;
+
+void get_next_token(char **input) {
+    char *ptr = *input;
+    
+    // Skip whitespace
+    while (*ptr == ' ' || *ptr == '\n') ptr++;
+    
+    if (*ptr == '\0') {
+        printf("Token: EOF\n");
+        return;
+    }
+
+    if (isdigit(*ptr)) {
+        // Scan Integer
+        char buffer[100];
+        int i = 0;
+        while (isdigit(*ptr)) {
+            buffer[i++] = *ptr++;
+        }
+        buffer[i] = '\0';
+        printf("Token: INTEGER (%s)\n", buffer);
+    } 
+    else if (isalpha(*ptr)) {
+        // Scan Identifier
+        char buffer[100];
+        int i = 0;
+        while (isalnum(*ptr)) {
+            buffer[i++] = *ptr++;
+        }
+        buffer[i] = '\0';
+        printf("Token: IDENTIFIER (%s)\n", buffer);
+    } 
+    else {
+        // Scan Operator
+        printf("Token: OPERATOR (%c)\n", *ptr);
+        ptr++;
+    }
+    
+    *input = ptr; // Update the pointer
+}
